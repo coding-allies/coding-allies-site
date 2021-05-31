1453df459a9e08c3232ffc546c831193e21c849a
Aditi Mishra <aditimishra@unomaha.edu>
2021-05-30 19:15:43 -0500

    Inserting white screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Box } from '@material-ui/core';
+import whiteBackground from "../assets/white-background.jpg"
 
 class Home extends Component {
   render() {
     return (
       <Box>
-        <h5>Home</h5>
-        <p>HOME PAGE Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <img src={whiteBackground} width="80%" alt="White Background Image" />
       </Box>
     );
   }
