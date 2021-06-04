a8af9372f66dd70e079fc72d5c7ed73cebc641b8
mimimysam <59676205+mimimysam@users.noreply.github.com>
2021-06-03 21:35:02 -0700

    created form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,128 @@
 import React, { Component } from 'react';
-import { Box } from '@material-ui/core';
+import { Box, TextField, MenuItem } from '@material-ui/core';
 
 class Contact extends Component {
+
+  state = {
+    topic: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: ''
+  }
+
+  topics = [
+    {
+      value: 'Volunteering',
+      label: 'Volunteering',
+    },
+    {
+      value: 'Company Partnership',
+      label: 'Company Partnership',
+    },
+    {
+      value: 'Other',
+      label: 'Other',
+    },
+  ];
+
+  handleTopicInput = (e:any) => {
+    this.setState({ topic: e.target.value });
+  };
+  handleFirstNameInput = (e:any) => {
+    this.setState({ firstName: e.target.value });
+  };
+  handleLastNameInput = (e:any) => {
+    this.setState({ lastName: e.target.value });
+  };
+  handleEmailInput = (e:any) => {
+    this.setState({ email: e.target.value });
+  };
+  handleMessageInput = (e:any) => {
+    this.setState({ message: e.target.value });
+  };
+
+  logInfo = (e:any) => {
+    e.preventDefault();
+    console.log(this.state.topic);
+    console.log(this.state.firstName);
+    console.log(this.state.lastName);
+    console.log(this.state.email);
+    console.log(this.state.message);
+  };
+
   render() {
     return (
       <Box>
         <h5>Contact Us</h5>
-        <p>CONTACT US PAGE Test Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>From will be included here</p>
+        <p>Please use this form to contact us. Alternatively, feel free to send us an email at admin@codingallies.org.</p>
+        <form>
+          <TextField
+            id="outlined-select-topic"
+            select
+            label="Select"
+            value={this.state.topic}
+            onChange={this.handleTopicInput}
+            // helperText="Please select your topic"
+            variant="outlined"
+          >
+            {this.topics.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <br></br>
+          <TextField
+            required
+            id="outlined-required"
+            label="First Name"
+            // defaultValue=""
+            value={this.state.firstName}
+            onChange={this.handleFirstNameInput}
+            variant="outlined"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Last Name"
+            // defaultValue=""
+            value={this.state.lastName}
+            onChange={this.handleLastNameInput}
+            variant="outlined"
+          />
+          <br></br>
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            // defaultValue=""
+            value={this.state.email}
+            onChange={this.handleEmailInput}
+            variant="outlined"
+          />
+          <br></br>
+          <TextField
+            required
+            id="outlined-required"
+            label="Message"
+            // defaultValue=""
+            value={this.state.message}
+            onChange={this.handleMessageInput}
+            variant="outlined"
+          />
+          <br></br>
+          <button
+            className="btn btn-large right"
+            onClick={this.logInfo}
+          >
+            SUBMIT
+          </button>
+        </form>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"</p>
       </Box>
     );
   }
 }
+
 export default Contact;
