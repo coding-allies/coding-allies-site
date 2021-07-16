78acbe56fc1fe5909785963215866ca89ffed581
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2021-07-15 19:32:25 -0700

    Moved logo to assets folder

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Button, Grid, Input, Container, Typography, Link, Icon, Paper, CardMedia } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
-import logo from "logo.png"
+import logo from "../assets/ca_single_flower_logo.png"
 
 const useStyles = makeStyles((theme) => ({
 	footer: {
@@ -26,56 +26,56 @@ function ButtonStyled() {
 }
 
 function Footer() {
-	const classes = useStyles();
-	return (
-		<Container component="footer" className={classes.footer}>
-			<Grid container spacing={2}>
-				<Grid item xs={4}>
-					<text>
-						Sign up for event updates
+  const classes = useStyles();
+  return (
+    <Container component="footer" className={classes.footer}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <text>
+            Sign up for event updates
 					</text>
-					<div>
-						<input
-							type="email"
-							placeholder="Email address"
-							color="primary"
-							style={{ borderRadius: 15 }}>
-						</input>
-						<Button
-							size="small"
-							variant="contained"
-							color="primary"
-							style={{ borderRadius: 15 }}>
-							Sign up
+          <div>
+            <input
+              type="email"
+              placeholder="Email address"
+              color="primary"
+              style={{ borderRadius: 15 }}>
+            </input>
+            <Button
+              size="small"
+              variant="contained"
+              color="primary"
+              style={{ borderRadius: 15 }}>
+              Sign up
 						</Button>
-					</div>
-				</Grid>
-				<Grid item xs={4}>
-						<img src={logo} alt="Coding Allies Logo" />
-					<Typography align="center">
-						&copy; Coding Allies {new Date().getFullYear()}{"."}
-					</Typography>
-				</Grid>
-				<Grid item xs={4}>
-					<text>
-						Follow us on:
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <img src={logo} width={50} alt="Coding Allies Logo" />
+          <Typography align="center">
+            &copy; Coding Allies {new Date().getFullYear()}{"."}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <text>
+            Follow us on:
 					</text>
-					<Link href="#" color="primary" variant="subtitle1">
-						Facebook
+          <Link href="#" color="primary" variant="subtitle1">
+            Facebook
 					</Link>
-					<Link href="#" onClick={preventDefault} variant="subtitle1">
-						Twitter
+          <Link href="#" onClick={preventDefault} variant="subtitle1">
+            Twitter
 					</Link>
-					<Link href="#" variant="subtitle1">
-						Insta
+          <Link href="#" variant="subtitle1">
+            Insta
 					</Link>
-					<Link href="#" variant="subtitle1">
-						LinkedIn
+          <Link href="#" variant="subtitle1">
+            LinkedIn
 					</Link>
-				</Grid>
-			</Grid>
-		</Container>
-	);
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Footer;
