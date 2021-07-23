c129c34903b7ea6df4d7e4f37139e392eb497e27
AayVer <83441396+AayVer@users.noreply.github.com>
2021-07-23 07:20:40 +0530

    width change

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -47,7 +47,7 @@ class About extends Component {
   //   description={"ABOUT PAGE Main Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
   // />
   <>
-<div style={{backgroundImage:'url('+img+')',width:"10000px",height:"450px"}}>
+<div style={{backgroundImage:'url('+img+')',width:"10000px",height:"400px"}}>
   <div style={{backgroundColor:"mistyrose",width:"340px", opacity:"0.8",marginTop:"30px"}}>
 <Typography gutterBottom variant="h5" component="h2">Coding Allies is an all-volunteer</Typography>
 </div>
