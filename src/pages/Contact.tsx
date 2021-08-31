e44d32bd98a97710c2baa7a8c3510d046a0bc9ed
z-mojomoji <suphicha.anu@gmail.com>
2021-08-31 13:17:23 +0700

    Fixed up the form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import axios from 'axios';
-import { Box, TextField, MenuItem } from '@material-ui/core';
+import { Box, TextField, MenuItem, Container } from '@material-ui/core';
 import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import './Contact.css';
@@ -13,6 +13,14 @@ const styles = (theme: Theme) => createStyles({
   },
   hilightText: {
     color: "red"
+  },
+  inputField: {
+    margin: "10px 5px",
+    width: "calc(100% - 10px)",
+  },
+  halfCol: {
+    display: "inline-block",
+    width: "50%",
   }
 });
 
@@ -67,17 +75,21 @@ class Contact extends Component<Props> {
   render() {
     const { classes } = this.props;
     return (
-      <Box>
+      <Container maxWidth="lg">
         <h1>Contact Us</h1>
         <p className={classes.hilightText}>Please use this form to contact us. Alternatively, feel free to send us an email at admin@codingallies.org.</p>
+        <Container maxWidth="sm">
         <form onSubmit={this.handleSubmit}>
           <TextField
-            id="topic"
             select
-            label="Select"
+            fullWidth
+            id="topic"
+            label="Topic"
+            defaultValue="Select"
             onChange={this.handleChange}
-            variant="outlined"
             name="entry.1519230818"
+            variant="outlined"
+            className={classes.inputField}
           >
             {this.topics.map((option) => (
               <MenuItem key={option.value} value={option.value}>
@@ -85,40 +97,53 @@ class Contact extends Component<Props> {
               </MenuItem>
             ))}
           </TextField>
-          <br></br>
-          <TextField
-            required
-            id="firstName"
-            label="First Name"
-            onChange={this.handleChange}
-            variant="outlined"
-            name="entry.1466030423"
-          />
+          <Box className={classes.halfCol}>
+            <TextField
+              required
+              id="firstName"
+              label="First Name"
+              defaultValue="&nbsp;"
+              onChange={this.handleChange}
+              name="entry.1466030423"
+              variant="outlined"
+              className={classes.inputField}
+            />
+          </Box>
+          <Box className={classes.halfCol}>
           <TextField
             required
             id="lastName"
             label="Last Name"
+            defaultValue="&nbsp;"
             onChange={this.handleChange}
-            variant="outlined"
             name="entry.363139444"
+            variant="outlined"
+            className={classes.inputField}
           />
-          <br></br>
+          </Box>
           <TextField
             required
+            fullWidth
             id="email"
             label="Email"
+            defaultValue="&nbsp;"
             onChange={this.handleChange}
-            variant="outlined"
             name="entry.1540871534"
+            variant="outlined"
+            className={classes.inputField}
           />
           <br></br>
           <TextField
             required
+            fullWidth
+            multiline
             id="message"
             label="Message"
+            defaultValue="&nbsp;"
             onChange={this.handleChange}
-            variant="outlined"
             name="entry.834565993"
+            variant="outlined"
+            className={classes.inputField}
           />
           <br></br>
           <button
@@ -128,8 +153,9 @@ class Contact extends Component<Props> {
             SUBMIT
           </button>
         </form>
+        </Container>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"</p>
-      </Box>
+      </Container>
     );
   }
 }
