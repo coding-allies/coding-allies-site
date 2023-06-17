1adeca3bddaf86dfee85841ce3352b97870b7ac1
Rey Dekker <shakhanova.n@gmail.com>
2023-06-16 17:02:44 -0700

    Made cards clickable & formatted

--- src/components/DataStructureAndAlgorithms.tsx
+++ src/components/DataStructureAndAlgorithms.tsx
@@ -1,38 +1,64 @@
-import { Grid, Container, Typography, Link, AppBar, Box, Button, CardMedia } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import {
+  Grid,
+  Container,
+  Typography,
+  Link,
+  AppBar,
+  Box,
+  Button,
+  CardMedia,
+  CardActionArea,
+} from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import { makeStyles } from "@material-ui/core/styles";
-import cardImage from "../assets/dataStructureAndAlgorithms.png"
-import AlertDialog from './AlertDialog';
+import cardImage from "../assets/dataStructureAndAlgorithms.png";
+import AlertDialog from "./AlertDialog";
 
 const useStyles = makeStyles((theme) => ({
-	media: {
-		height: 0,
-		padding: '30.25%', // 16:9,
-	  },
-	  homeCard: {
-		width:`calc(20vw)`,
-		margin:'50px',
-		alignItems: 'center'
-	  },
-	  text:{
-		margin: '20px', 
-		fontFamily:'Montserrat',
-		fontWeight: 600
-	  }
+  media: {
+    height: 0,
+    padding: "30.25%", // 16:9,
+  },
+  homeCard: {
+    width: `calc(20vw)`,
+    margin: "50px",
+    alignItems: "center",
+  },
+  text: {
+    margin: "20px",
+    fontFamily: "Montserrat",
+    fontWeight: 600,
+  },
 }));
 
 const DataStructureAndAlgorithms = () => {
-	const classes = useStyles();
-	return (
-		<Card className={classes.homeCard} variant="outlined">
-			<CardContent>
-              <CardMedia className={classes.media} image={cardImage} title="Contemplative Reptile"/>
-              <Typography className={classes.text}>Practice free mock interview with your peers in a beginner-friendly safe environment.</Typography>
-			  </CardContent>
-            </Card>
-	);
-}
+  const classes = useStyles();
+  const handleLinkClick = () => {
+    window.open(
+      "https://www.facebook.com/groups/codingallies/events",
+      "_blank"
+    );
+  };
+
+  return (
+    <Card className={classes.homeCard} variant="outlined">
+      <CardActionArea onClick={handleLinkClick}>
+        <CardMedia
+          className={classes.media}
+          image={cardImage}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography className={classes.text}>
+            Practice free mock interview with your peers in a beginner-friendly
+            safe environment.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 export default DataStructureAndAlgorithms;
--- src/components/DiscordCommunity.tsx
+++ src/components/DiscordCommunity.tsx
@@ -1,37 +1,55 @@
-import { Typography, Button, CardMedia } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import {
+  Typography,
+  Button,
+  CardMedia,
+  CardActionArea,
+} from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import { makeStyles } from "@material-ui/core/styles";
-import cardImage from "../assets/community.png"
+import cardImage from "../assets/community.png";
 
 const useStyles = makeStyles((theme) => ({
-	media: {
-		height: 0,
-		padding: '30.25%',
-	  },
-	  homeCard: {
-		width:`calc(20vw)`,
-		margin:'50px',
-		alignItems: 'center'
-	  },
-	  text:{
-		margin: '20px', 
-		fontFamily:'Montserrat',
-		fontWeight: 600
-	  }
+  media: {
+    height: 0,
+    padding: "30.25%",
+  },
+  homeCard: {
+    width: `calc(20vw)`,
+    margin: "50px",
+    alignItems: "center",
+  },
+  text: {
+    margin: "20px",
+    fontFamily: "Montserrat",
+    fontWeight: 600,
+  },
 }));
 
 const DiscordCommunity = () => {
-	const classes = useStyles();
-	return (
-		<Card className={classes.homeCard} variant="outlined">
-			<CardContent>
-              <CardMedia className={classes.media} image={cardImage} title="Community"/>
-              <Typography className={classes.text}>Become part of our online community on Discord & Facebook and find our upcoming events.</Typography>
-			  </CardContent>
-            </Card>
-	);
-}
+  const classes = useStyles();
+  const handleLinkClick = () => {
+    // Discord Invite that won't expire
+    window.open("https://discord.gg/qd9GZhKtdY", "_blank");
+  };
+
+  return (
+    <Card className={classes.homeCard} variant="outlined">
+      <CardActionArea onClick={handleLinkClick}>
+        <CardMedia
+          className={classes.media}
+          image={cardImage}
+          title="Community"
+        />
+        <CardContent>
+          <Typography className={classes.text}>
+            Become part of our online community on Discord, practice DSA & find
+            our upcoming events.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 export default DiscordCommunity;
